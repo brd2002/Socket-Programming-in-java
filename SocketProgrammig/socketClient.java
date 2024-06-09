3132f8e22aef8773f45f982355fc89f319f6e122
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-06-09 20:42:15 +0530

    learning socket programming

---
+++ SocketProgrammig/socketClient.java
@@ -0,0 +1,25 @@
+package SocketProgrammig;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class socketClient {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Client started....");
+            Socket soc = new Socket("localhost", 9806);
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Please enter your name 😊");
+            String str = userInput.readLine();
+            PrintWriter out = new PrintWriter(soc.getOutputStream() , true) ; 
+            out.println(str);
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            System.out.println(in.readLine());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ SocketProgrammig/socketSever.java
@@ -0,0 +1,26 @@
+package SocketProgrammig;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
+
+public class socketSever {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Waiting for clients......");
+            ServerSocket ss = new ServerSocket(9806);
+            Socket soc = ss.accept();
+            System.out.println("Connection established.......");
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            String str = in.readLine();
+            PrintWriter out = new PrintWriter(soc.getOutputStream() , true);
+            out.println("Sever says "+ str) ; 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
